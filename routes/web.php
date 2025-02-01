92106e02812dabf0a7687740203a3e87bc0bd66f
Zakhar Dudych <michadyduch@gmail.com>
2025-02-01 19:07:57 +0200

    routes update during tests

--- routes/web.php
+++ routes/web.php
@@ -12,22 +12,23 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::group(['prefix' => 'companies'], function () {
+        Route::get('/', [CompanyController::class, 'index'])->name('companies.index');
+        Route::get('/create', [CompanyController::class, 'create'])->name('companies.create');
+        Route::get('/{company}', [CompanyController::class, 'show'])->name('companies.show');
+        Route::post('/', [CompanyController::class, 'store'])->name('companies.store');
+        Route::put('/{company}', [CompanyController::class, 'update'])->name('companies.update');
+        Route::get('/{company}/edit', [CompanyController::class, 'edit'])->name('companies.edit');
+        Route::delete('/{company}', [CompanyController::class, 'destroy'])->name('companies.delete');
+    });
+    Route::group(['prefix' => 'employees'], function () {
+        Route::get('/create/{company}', [EmployeeController::class, 'create'])->name('employees.create');
+        Route::post('/store/{company}', [EmployeeController::class, 'store'])->name('employees.store');
+        Route::get('/{employee}', [EmployeeController::class, 'show'])->name('employees.show');
+        Route::put('/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
+        Route::get('/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
+        Route::delete('/{employee}', [EmployeeController::class, 'destroy'])->name('employees.delete');
+    });
+});
 
-Route::group(['prefix' => 'companies'], function () {
-    Route::get('/', [CompanyController::class, 'index'])->name('companies.index');
-    Route::get('/{company}', [CompanyController::class, 'show'])->name('companies.show');
-    Route::post('/', [CompanyController::class, 'store'])->name('companies.store');
-    Route::get('/create', [CompanyController::class, 'create'])->name('companies.create');
-    Route::put('/{company}', [CompanyController::class, 'update'])->name('companies.update');
-    Route::get('/{company}/edit', [CompanyController::class, 'edit'])->name('companies.edit');
-    Route::delete('/{company}', [CompanyController::class, 'destroy'])->name('companies.delete');
-});
-Route::group(['prefix' => 'employees'], function () {
-    Route::get('/', [EmployeeController::class, 'index'])->name('employees.index');
-    Route::get('/{employee}', [EmployeeController::class, 'show'])->name('employees.show');
-    Route::post('/', [EmployeeController::class, 'store'])->name('employees.store');
-    Route::get('/create', [EmployeeController::class, 'create'])->name('employees.create');
-    Route::put('/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
-    Route::get('/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
-    Route::delete('/{employee}', [EmployeeController::class, 'destroy'])->name('employees.delete');
-});
