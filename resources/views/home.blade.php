f1de332a6d684e76eb88d9591e7cb3003f399944
Zakhar Dudych <michadyduch@gmail.com>
2025-02-01 19:06:20 +0200

    minor edits of default views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <div class="card-header">{{ __('Dashboard') }}
+                    <div class="btn-group justify-content-between float-end">
+                        <a href="{{ route('companies.index') }}" class="btn btn-primary">
+                            {{ __('Companies') }}
+                        </a>
+                    </div>
+                </div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
