668977f9482adc733523fc84689e378b332f06b9
Zakhar Dudych <michadyduch@gmail.com>
2025-02-01 13:55:11 +0200

    basic auth for administrator

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
